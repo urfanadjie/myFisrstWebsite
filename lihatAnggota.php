30f52c3ad332e467d17c060e05615bb246ac4528
Adjie Pratama <85275092+urfanadjie@users.noreply.github.com>
2021-06-03 13:14:58 +0700

    Update lihatAnggota.php

--- lihatAnggota.php
+++ lihatAnggota.php
@@ -12,9 +12,7 @@
 <head>
 
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
-    <script src="bootstrap/js/jquery-3.2.1.js"></script>
-    <script src="bootstrap/js/bootstrap.min.js"></script>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
 	<title>Data Anggota</title>
 
 </head>
